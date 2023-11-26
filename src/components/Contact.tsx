26cd275e2bb377417a3b82bc3b2b918e1a927cbd
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-26 11:54:08 -0300

    main work

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,18 @@
 const Contact = () => {
     return (
         <div className="flex flex-col mb-10">
-            <h3 className="text-1xl font-bold mb-2">Contact</h3>
+            <h3 className="text-1xl font-bold mb-2">Say hello</h3>
 
             <div className="flex flex-row mb-10 space-x-24">
-                <div className="flex flex-col mb-10">
+                <div className="flex flex-col space-y-1">
                     <p className="text-gray">Email</p>
-                    <p className="text-gray">Github</p>
+                    <p className="text-gray">GitHub</p>
+                    <p className="text-gray">Linkedin</p>
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col space-y-1">
                     <p className="text-gray">aguirre.brianalexis94@gmail.com</p>
-                    <p className="text-gray">github.com/braiaguirre</p>
+                    <p className="text-gray">/braiaguirre</p>
+                    <p className="text-gray">/brian-aguirre-1947a3281</p>
                 </div>
             </div>
         </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import Project from './Project'
 
 const Projects = () => {
     return (
-        <div className="flex flex-col mb-14">
+        <div className="flex flex-col mb-24">
             <h3 className="text-1xl font-bold mb-2">Projects</h3>
             <Project title="Creva" desc="Pokémon project using PokeApiusing PokeApi." />
             <Project title="Poké Henry" desc="Pokémon project using PokeApi." />
