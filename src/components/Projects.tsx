c9b5310060ce3132ed15c0e03d71532360c2c3e2
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-29 17:20:15 -0300

    text desc fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,22 +8,22 @@ const Projects = () => {
             <h3 className="text-1xl font-bold mb-2 text-title">Projects</h3>
             <Project 
                 title="Portfolio" 
-                desc="Private repo, sorry." 
+                desc="Click to open." 
                 url="https://github.com/braiaguirre/"
             />
             <Project 
                 title="Creva" 
-                desc="Private repo, sorry." 
+                desc="Private repository." 
                 url="https://github.com/braiaguirre/" 
             />
             <Project 
                 title="Pokéapi" 
-                desc="Click to open repo." 
+                desc="Click to open." 
                 url="https://github.com/braiaguirre/pi-pokemon" 
             />
             <Project 
                 title="Rick and Morty" 
-                desc="Click to open repo." 
+                desc="Click to open." 
                 url="https://github.com/braiaguirre/rick-and-morty" 
             />
         </div>
