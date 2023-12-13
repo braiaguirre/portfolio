47a6592409756dc77c5e6ab3ffe71c0a2957f903
braiaguirre <brianaguirre.sp94@gmail.com>
2023-12-13 16:57:57 -0300

    i18n installed

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(initReactI18next);
+i18n.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    en: {
+        translation: {
+            hello: 'Hello.'
+        }
+    },
+    es: {
+        translation: {
+            hello: 'Hola.'
+        }
+    }
+  },
+});
 
 export default i18n;
