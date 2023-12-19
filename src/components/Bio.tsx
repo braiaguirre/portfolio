349fda29f55bbe1c592aa6fff9216cdbe7756d8d
braiaguirre <brianaguirre.sp94@gmail.com>
2023-12-19 14:06:21 -0300

    i18n test

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next"
+
 const Bio = () => {
+    const { t } = useTranslation();
     return (
         <div className="flex flex-col mb-24">
-            <h1 className="text-3xl font-bold mb-2 text-title">I'm Brian,</h1>
+            <h1 className="text-3xl font-bold mb-2 text-title">{t('hello')}I'm Brian,</h1>
             <h2 className="text-3xl font-bold text-gray mb-4">a full stack developer based in Bariloche, Argentina.</h2>
             <p className='text-lightgray'>Full stack developer with a knack for creating innovative applications. I thrive in fast-paced environments and enjoy collaborating to bring ideas to life. When not coding, I may be found trekking around Bariloche.</p>
         </div>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next"
 import Navbar from '../components/Navbar'
 import Bio from '../components/Bio'
 import Links from '../components/Links'
@@ -5,13 +6,15 @@ import Contact from '../components/Contact'
 import db from '../utils/db.json'
 
 const Home = () => {
+    const { t } = useTranslation();
+    
     return (
       <div className="flex flex-col w-[400px] md:w-[550px] lg:w-[650px]">
           <Navbar />
           <Bio />
-          <Links title="Projects" links={ db.projects } />
+          <Links title={t('projects')} links={ db.projects } />
           <Links title="Kiwee Studio" links={ db.kiwee } />
-          <Links title="Others" links={ db.others } />
+          <Links title={t('others')} links={ db.others } />
           <Contact />
       </div>
     )
