f06ed71ea1ef2cdb89eea5d5f2b31f275cecd849
braiaguirre <brianaguirre.sp94@gmail.com>
2023-12-19 14:11:51 -0300

    i18n test

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -4,9 +4,9 @@ const Bio = () => {
     const { t } = useTranslation();
     return (
         <div className="flex flex-col mb-24">
-            <h1 className="text-3xl font-bold mb-2 text-title">{t('hello')}I'm Brian,</h1>
-            <h2 className="text-3xl font-bold text-gray mb-4">a full stack developer based in Bariloche, Argentina.</h2>
-            <p className='text-lightgray'>Full stack developer with a knack for creating innovative applications. I thrive in fast-paced environments and enjoy collaborating to bring ideas to life. When not coding, I may be found trekking around Bariloche.</p>
+            <h1 className="text-3xl font-bold mb-2 text-title">{t('bioTitle')}</h1>
+            <h2 className="text-3xl font-bold text-gray mb-4">{t('bioSubtitle')}</h2>
+            <p className='text-lightgray'>{t('bioDesc')}</p>
         </div>
     )
 }
